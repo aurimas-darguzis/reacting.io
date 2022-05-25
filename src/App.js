347b534e5557b94c0f56048de168436ecd7d653c
aurimas-darguzis <aurimasdarguzis@gmail.com>
2022-05-25 21:28:54 +0100

    its coming

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Cominning soon...</p>
+        <p>😁 coming soon 😁</p>
       </header>
     </div>
   )
