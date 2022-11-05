d07d72d0e9ac60f05d8c9717137d6d1373ac3694
aurimas-darguzis <aurimasdarguzis@gmail.com>
2022-11-05 12:55:28 +0000

    add logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+
+import reacting from "./assets/reactingio.jpg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Coming soon</p>
+        <img src={reacting} className="App-logo" alt="logo" />
       </header>
     </div>
   );
